f9fb085378e7a3fc24a330db390db2ac94fad863
Anna <annarsve@gmail.com>
2022-08-18 17:25:51 +0500

    index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,32 @@
-var popularGoodsSwiper = new Swiper(".popularGoods", {
+var goodsSwiper = new Swiper(".goodsSwiper", {
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-    slidesPerView: 5,
-    spaceBetween: 83,
+    slidesPerView: 2,
+    spaceBetween: 8,
+    breakpoints: {
+        // when window width is >= 480px
+        912: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        1370: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        },
+        1740: {
+            slidesPerView: 5,
+            spaceBetween: 83,
+        }
+    }
 });
+
+var cityBikeSlider = new Swiper(".cityBikeSlider", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
